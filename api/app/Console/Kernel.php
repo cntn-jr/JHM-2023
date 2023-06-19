2ec674617d811746d7d925678d39c436a23c1ea2
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-06-19 23:04:24 +0900

    style: :art:コードのフォーマット

--- api/app/Console/Kernel.php
+++ api/app/Console/Kernel.php
@@ -9,6 +9,8 @@ class Kernel extends ConsoleKernel
 {
     /**
      * Define the application's command schedule.
+     *
+     * @param Schedule $schedule
      */
     protected function schedule(Schedule $schedule): void
     {
--- api/app/Http/Middleware/Authenticate.php
+++ api/app/Http/Middleware/Authenticate.php
@@ -9,6 +9,8 @@ class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
+     *
+     * @param Request $request
      */
     protected function redirectTo(Request $request): ?string
     {
