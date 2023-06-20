1edddb89c4c50a7bd5907303d11de486ed2ee1c8
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-06-20 22:12:21 +0900

    feat: :sparkles:就活ステータスのモデルとコントローラー、APIリソース、APIコレクション、ファクトリー、マイグレーション、シーダー

---
+++ api/database/seeders/ActivityStatusSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ActivityStatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
