046559c639b4246d7ff031b88ada493a9f1efbf5
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-07-01 17:39:23 +0900

    feat: :sparkles:出席番号のデータ形式を変更

--- api/database/migrations/2014_10_12_000000_create_users_table.php
+++ api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->integer('attendance_number');
+            $table->integer('attendance_number')->nullable();
             $table->integer('role');
             $table->timestamps();
         });
