6e40d6847f3190c3dd370b04c5c5a06d3dea30f0
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-07-02 12:16:36 +0900

    feat: :sparkles:就活のステータスにカラムを追加

--- api/database/migrations/2023_06_20_131024_create_activity_statuses_table.php
+++ api/database/migrations/2023_06_20_131024_create_activity_statuses_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('activity_statuses', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->text('note');
             $table->timestamps();
         });
     }
