ce1b3b9d674230dc20be357f6c2b28575ab04f12
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-08-06 11:27:09 +0900

    feat: :sparkles:管理者用のAPIの定義

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthenticateController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SchoolController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('administrator')->group(function () {
+    // ログイン
+    Route::get('/login', [AuthenticateController::class, 'loginAdministrator']);
+    Route::middleware(['auth:sanctum'])->group(function () {
+        // ホーム
+        Route::get('/', [HomeController::class, 'index']);
+        // 学校関連
+        Route::prefix('school')->group(function () {
+            Route::get('/', [SchoolController::class, 'index']);
+            Route::post('/', [SchoolController::class, 'store']);
+            Route::get('/{id}', [SchoolController::class, 'show']);
+            Route::put('/{id}', [SchoolController::class, 'update']);
+            Route::delete('/{id}', [SchoolController::class, 'destroy']);
+        });
+    });
 });
