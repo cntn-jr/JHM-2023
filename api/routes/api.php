747a6e4421a7c7c7d6e2253076dd4dad78f1164c
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-08-11 08:33:02 +0900

    feat: :sparkles:学校管理者用のAPIの定義

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\AuthenticateController;
+use App\Http\Controllers\HavingClassController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SchoolClassController;
 use App\Http\Controllers\SchoolController;
+use App\Http\Controllers\TeacherController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -30,6 +33,39 @@ Route::prefix('administrator')->group(function () {
             Route::get('/{id}', [SchoolController::class, 'show']);
             Route::put('/{id}', [SchoolController::class, 'update']);
             Route::delete('/{id}', [SchoolController::class, 'destroy']);
+        });
+    });
+});
+
+// 学校における管理者
+Route::prefix('manager')->group(function () {
+    // ログイン
+    Route::get('/login', [AuthenticateController::class, 'loginManager']);
+    Route::middleware(['auth:sanctum'])->group(function () {
+        // ホーム
+        Route::get('index', [HomeController::class, 'index']);
+        // 教師情報の管理
+        Route::prefix('teacher')->group(function () {
+            Route::get('/', [TeacherController::class, 'index']);
+            Route::post('/', [TeacherController::class, 'store']);
+            Route::post('/finalize', [TeacherController::class, 'finalize']);
+            Route::put('/{id}', [TeacherController::class, 'update']);
+            Route::delete('/{id}', [TeacherController::class, 'destroy']);
+        });
+        // クラス情報の管理
+        Route::prefix('class')->group(function () {
+            Route::get('/', [SchoolClassController::class, 'index']);
+            Route::post('/', [SchoolClassController::class, 'store']);
+            Route::post('/finalize', [SchoolClassController::class, 'finalize']);
+            Route::put('/{id}', [SchoolClassController::class, 'update']);
+            Route::delete('/{id}', [SchoolClassController::class, 'destroy']);
+            // クラス担任の管理
+            Route::prefix('assign')->group(function () {
+                Route::post('/', [HavingClassController::class, 'store']);
+                Route::post('/finalize', [HavingClassController::class, 'finalize']);
+                Route::put('/{id}', [HavingClassController::class, 'update']);
+                Route::delete('/{id}', [HavingClassController::class, 'destroy']);
+            });
         });
     });
 });
