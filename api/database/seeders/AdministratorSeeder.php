c06eee01ffad913b7db68a68f83243570259086e
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-08-12 14:35:49 +0900

    feat: :sparkles:ユーザーシーダーを作成

--- api/database/seeders/AdministratorSeeder.php
+++ api/database/seeders/AdministratorSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Database\Factories\AdministratorFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,8 @@ class AdministratorSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::factory()->create([
+            'email' => 'admin@example.com',
+        ]);
     }
 }
