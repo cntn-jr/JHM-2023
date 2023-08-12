4e0af3d6864be6821da57523d61b04eb064764c9
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-08-12 11:53:39 +0900

    feat: :sparkles:システム管理者ファクトリーを作成

--- api/database/factories/AdministratorFactory.php
+++ api/database/factories/AdministratorFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Role;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -17,7 +19,9 @@ class AdministratorFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'email' => fake()->unique()->userName() . '@admin.kawahara.ac.jp',
+            'password' => Hash::make('password'),
+            'role' => Role::ADMINISTRATOR,
         ];
     }
 }
