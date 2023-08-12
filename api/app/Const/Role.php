159d331d8256a237719598917b4efef6d6d797ef
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-08-12 11:44:02 +0900

    feat: :sparkles:ロール定数一覧を作成

---
+++ api/app/Const/Role.php
@@ -0,0 +1,15 @@
+<?php
+
+class Role {
+    public const ADMINISTRATOR = 1;
+    public const MANAGER = 2;
+    public const TEACHER = 3;
+    public const STUDENT = 4;
+
+    public const ROLES = [
+        self::ADMINISTRATOR,
+        self::MANAGER,
+        self::TEACHER,
+        self::STUDENT,
+    ];
+}
