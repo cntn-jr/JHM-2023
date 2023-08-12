2231dcf8b30cbad27476a23628d0ce4106761fdd
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-08-12 14:32:48 +0900

    feat: :sparkles:ユーザーシーダーを役割ごとに分割

--- api/database/seeders/TeacherSeeder.php
+++ api/database/seeders/TeacherSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class UserSeeder extends Seeder
+class TeacherSeeder extends Seeder
 {
     /**
      * Run the database seeds.
