7ff2666325c46d27746b6e36ae00dd9bfb8dc06b
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-08-12 16:03:43 +0900

    fix: :adhesive_bandage:モデルのリレーションを追加

--- api/app/Models/Administrator.php
+++ api/app/Models/Administrator.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Administrator extends User
 {
     use HasFactory;
+
+    protected $table = 'users';
 }
--- api/app/Models/Student.php
+++ api/app/Models/Student.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Student extends User
 {
     use HasFactory;
+
+    protected $table = 'users';
+
+    public function school(): BelongsTo
+    {
+        return $this->belongsTo(Manager::class);
+    }
 }
