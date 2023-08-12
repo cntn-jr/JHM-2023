7dbe345b89523219800e8d4ffd0ffc843f7a5c6c
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-08-13 08:47:03 +0900

    fix: :adhesive_bandage:学校と企業のリレーションを追加

--- api/app/Models/School.php
+++ api/app/Models/School.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class School extends Model
@@ -24,5 +25,15 @@ class School extends Model
     public function students(): HasMany
     {
         return $this->hasMany(Student::class);
+    }
+
+    public function companies(): HasManyThrough
+    {
+        return $this->hasManyThrough(
+            Company::class,
+            User::class,
+            'school_id',
+            'user_id',
+        );
     }
 }
