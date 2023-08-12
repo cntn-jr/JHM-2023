ac39a4c229b93bac6a505a992c6a30b732bcc69e
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-08-12 14:09:37 +0900

    feat: :sparkles:生徒ファクトリーの作成

--- api/database/factories/StudentFactory.php
+++ api/database/factories/StudentFactory.php
@@ -2,7 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\School;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Role;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -16,8 +19,16 @@ class StudentFactory extends Factory
      */
     public function definition(): array
     {
+        $schools = School::all(['id']);
         return [
-            //
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'first_name_kana' => fake()->firstKanaName(),
+            'last_name' => fake()->lastKanaName(),
+            'email' => fake()->userName() . '@stu.kawahara.ac.jp',
+            'password' => Hash::make('password'),
+            'role' => Role::STUDENT,
+            'school_id' => fake()->randomElement($schools)->id,
         ];
     }
 }
