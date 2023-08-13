eea25066e73deae3b9db960f3589bed8cd639764
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-08-13 09:13:14 +0900

    feat: :sparkles:会社ファクトリを作成

--- api/database/factories/CompanyFactory.php
+++ api/database/factories/CompanyFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Const\Role;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +18,16 @@ class CompanyFactory extends Factory
      */
     public function definition(): array
     {
+        $users = User::query()
+            ->whereIn([Role::STUDENT, Role::TEACHER])
+            ->get(['id']);
         return [
-            //
+            'name' => fake()->companyPrefix(),
+            'homepage_url' => 'https://google.com',
+            'job_hunting_app_url' => 'https://www.wantedly.com/projects',
+            'user_id' => fake()->randomElement($users)->id,
+            'head_office_location' => fake()->prefecture(),
+            'note' => fake()->emoji(),
         ];
     }
 }
