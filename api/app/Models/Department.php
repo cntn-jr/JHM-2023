08761b7a7d8f276e3eefc21b815f89559f0d9c29
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-08-27 15:46:40 +0900

    fix: :adhesive_bandage:学科モデルに学科長モデルとのリレーションを追加

--- api/app/Models/Department.php
+++ api/app/Models/Department.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Department extends Model
 {
@@ -13,5 +14,10 @@ class Department extends Model
     public function school() :BelongsTo
     {
         return $this->belongsTo(School::class);
+    }
+
+    public function departmentHeads() :HasMany
+    {
+        return $this->hasMany(DepartmentHead::class);
     }
 }
